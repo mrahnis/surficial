da1c49503945cec1395bfc0ef5c1837f7340dfed
Mike Rahnis <mike.rahnis@gmail.com>
2017-01-20 23:48:11 -0500

    remove commented section

--- surficial/cli/util.py
+++ surficial/cli/util.py
@@ -71,39 +71,6 @@ def read_geometries(feature_f):
         feature_crs = feature_src.crs_wkt
     return schema_geometry, feature_crs, geometries
 
-"""
-def read_geometries(feature_f, elevation_f=None, keep_z=False):
-
-    import click
-    import fiona
-    import rasterio
-    from shapely.geometry import shape
-
-    import drapery
-
-    with fiona.open(feature_f) as feature_src:
-        supported = ['Point', 'LineString', '3D Point', '3D LineString']
-        try:
-            if feature_src.schema['geometry'] not in supported:
-                raise click.BadParameter('Geometry must be one of: {}'.format(supported))
-        except:
-            raise click.BadParameter('Unable to obtain schema from {}'.format(feature_f))
-        if elevation_f and not keep_z:
-            with rasterio.open(elevation_f) as raster:
-                if feature_src.crs != raster.crs:
-                    raise click.BadParameter('{} and {} use different CRS'.format(feature_f, elevation_f))
-                geometries = [drapery.drape(raster, feature) for feature in feature_src]
-        else:
-            if feature_src.schema['geometry'] in ['LineString', 'Point'] and not keep_z:
-                raise click.BadParameter('{} is 2D. Provide an elevation source, or convert to 3D geometry'.format(feature_f))
-            geometries = [shape(feature['geometry']) for feature in feature_src]
-
-        feature_crs = feature_src.crs_wkt
-
-    return feature_crs, geometries
-"""
-
-
 def df_extents(df, xcol='x', ycol='y'):
     from collections import namedtuple
 
