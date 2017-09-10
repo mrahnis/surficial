2c74e48bed528cc1bb4a39dace14cc2032ad7bcb
Mike Rahnis <mike.rahnis@gmail.com>
2017-09-09 23:45:53 -0400

    Document matplotlib helper

--- surficial/tools/plotting.py
+++ surficial/tools/plotting.py
@@ -1,10 +1,23 @@
 from matplotlib.collections import LineCollection
 
 def vertices_to_linecollection(vertices, xcol='x', ycol='y', style=None):
+    """Return a matplotlib LineCollection given two pandas DataFrame columns
+
+    Parameters:
+        vertices (DataFrame): DataFrame containing xy data
+
+    Other Parameters:
+        x (string): x column name
+        y (string): y column name
+
+    Returns:
+        collection (LineCollection): matplotlib LineCollection
+
+    """
     verts = [list(zip(edge[xcol], edge[ycol])) for _, edge in vertices.groupby('edge')]
     if style != None:
-    	lines = LineCollection(verts, **style)
+    	collection = LineCollection(verts, **style)
     else:
-	    lines = LineCollection(verts)
-	    
-    return lines
+	    collection = LineCollection(verts)
+
+    return collection
