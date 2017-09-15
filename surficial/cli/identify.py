5be6984734387613dae81cdd25bcc9ca1caa9e29
Mike Rahnis <mike.rahnis@gmail.com>
2017-09-14 22:31:34 -0400

    Replace knickpoint filter with simpler approach
    Old approach moved to knickpoint_alt.

--- surficial/cli/identify.py
+++ surficial/cli/identify.py
@@ -50,6 +50,7 @@ def identify(ctx, alignment_f, output_f, elevation_f, densify, min_slope, min_dr
     alignment.vertices = despike
     vertices = surficial.slope(alignment, column='zmin')
     hits = surficial.knickpoint(vertices, min_slope, min_drop, up=up)
+
     print(hits)
 
     sink_schema = {
--- surficial/ops/filter.py
+++ surficial/ops/filter.py
@@ -1,6 +1,53 @@
 import numpy as np
+import pandas as pnd
+
 
 def knickpoint(vertices, min_slope, min_drop, up=True):
+    """Identify knickpoints given minimum slope and elevation drop
+
+    Shortcomings
+    * the slope series of interest must be entirely within the graph edge
+    * controlling it is fiddely by nature
+    
+    Parameters:
+        vertices (DataFrame): vertex coordinates
+        min_slope (float): slope as rise/run; negative slopes fall downstream
+        min_drop (float): minimum elevation drop required for identification 
+        up (boolean): return crest of slope (default) or toe of slope
+
+    Returns:
+        result (DataFrame): Datafrom records marking crest/toe of slopes meeting the given criteria with column for accumulated drop
+
+        :m (float): distance from the edge start endpoint
+        :x (float): x coordinate
+        :y (float): y coordinate
+        :z (float): z coordinate
+        :edge (tuple): pair of graph nodes (from, to)
+        :m_relative (float): distance from the outlet
+        :zmin (float): z where spikes have been removed by expanding min
+        :rise (float): change in specified column in the downstream direction
+        :slope (float): rise over run in the downstream direction 
+        :drop (float): max accumulated drop above min_drop over a slope steeper than min_slope
+
+    """
+    vertices['is_steep'] = np.where(vertices['slope'] <= min_slope, 0, 1)
+    # okay as long as slope doesn't terminate at an endpoint - need to expand
+    vertices['is_steep'] = vertices['is_steep'].shift(1)
+    vertices['series'] = vertices['is_steep'].cumsum()
+
+    ups = vertices.groupby('series').first()
+    downs = vertices.groupby('series').last()
+    drops = ups['zmin'] - downs['zmin']
+    drops.name = 'drop'
+    if up == True:
+        candidates = pnd.concat([ups, drops], axis=1)
+    else:
+        candidates = pnd.concat([downs, drops], axis=1)
+    result = candidates[candidates['drop'] >= min_drop]
+
+    return result
+
+def knickpoint_alt(vertices, min_slope, min_drop, up=True):
     """Identify knickpoints given minimum slope and elevation drop
 
     Shortcomings
@@ -12,6 +59,7 @@ def knickpoint(vertices, min_slope, min_drop, up=True):
         vertices (DataFrame): vertex coordinates
         min_slope (float): slope as rise/run; negative slopes fall downstream
         min_drop (float): minimum threshold elevation drop to identify a dam or knickpoint 
+        up (boolean): return crest of slope (default) or toe of slope
 
     Returns:
         result (DataFrame): Datafrom records marking toe of slopes meeting the given criteria with column for accumulated drop
