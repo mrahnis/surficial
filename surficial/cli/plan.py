8683481cd26dbfccb865d8f8d5fe4cdb684b58c0
Mike Rahnis <mike.rahnis@gmail.com>
2018-09-26 23:10:39 -0400

    Add basic labeling to plan subcommand

--- surficial/cli/plan.py
+++ surficial/cli/plan.py
@@ -1,11 +1,15 @@
 import click
 import matplotlib.pyplot as plt
 import pandas as pnd
+import fiona
+import numpy as np
+
 
 import surficial as srf
 from surficial.cli import defaults, util
 import surficial.tools.messages as msg
 from surficial.tools.plotting import vertices_to_linecollection
+from adjustText import adjust_text
 
 
 @click.command()
@@ -14,10 +18,12 @@ from surficial.tools.plotting import vertices_to_linecollection
               help='Plot points on the planview map using a given style')
 @click.option('--style', 'style', nargs=1, type=click.Path(exists=True),
               help="JSON file containing plot styles")
+@click.option('--label/--no-label', is_flag=True, default=False,
+              help="Label features from a given field in the features dataset")
 @click.option('--show-nodes/--hide-nodes', is_flag=True, default=False,
               help="Label network nodes in the alignment")
 @click.pass_context
-def plan(ctx, alignment, point_layers, style, show_nodes):
+def plan(ctx, alignment, point_layers, style, label, show_nodes):
     """
     Plots a planview map
 
@@ -40,6 +46,7 @@ def plan(ctx, alignment, point_layers, style, show_nodes):
         user_style = util.load_style(style)
         styles.update(user_style)
 
+    texts = []
     handles = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -57,12 +64,25 @@ def plan(ctx, alignment, point_layers, style, show_nodes):
             else:
                 click.echo((msg.PROJECTION).format(point_layer, alignment))
 
+        xx = [p.x for p in point_geoms]
+        yy = [p.y for p in point_geoms]
+
         if 'left' and 'right' in styles.get(style_key):
             click.echo("Left and right styling not implemented in plan view; using left style only.")
-            points, = ax.plot([p.coords.xy[0] for p in point_geoms], [p.coords.xy[1] for p in point_geoms], **styles.get(style_key).get('left'))
+            points, = ax.plot(xx, yy, **styles.get(style_key).get('left'))
         else:
-            points, = ax.plot([p.coords.xy[0] for p in point_geoms], [p.coords.xy[1] for p in point_geoms], **styles.get(style_key))
+            points, = ax.plot(xx, yy, **styles.get(style_key))
         handles.append(points)
+
+        if label:
+            with fiona.open(point_layer) as point_src:
+                if 'LABEL' in (point_src.schema)['properties']:
+                    labels = [feature['properties']['LABEL'] for feature in point_src]
+                else:
+                    labels = [feature['properties']['id'] for feature in point_src]
+                _texts = [ax.text(x, y, tx, clip_on=True, fontsize='small') for x, y, tx
+                          in zip(xx, yy, labels)]
+            texts.extend(_texts)
 
     if show_nodes:
         nodes = network.nodes(data=True)
@@ -90,5 +110,10 @@ def plan(ctx, alignment, point_layers, style, show_nodes):
            ylim=(extents.miny - pady, extents.maxy + pady),
            xlabel='Easting ({})'.format(unit.lower()),
            ylabel='Northing ({})'.format(unit.lower()))
+
+    if label:
+        iters = adjust_text(texts, ax=ax, arrowprops=dict(arrowstyle="-", color='r', lw=0.5))
+        print(iters, 'iterations')
+
     plt.legend(handles=handles)
     plt.show()
